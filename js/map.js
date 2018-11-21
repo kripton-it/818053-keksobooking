9cb6469ce6d4a9370142479a37260470a01dc6f9
Mikhail Shardin <kriptonit1988@gmail.com>
2018-11-21 21:27:32 +0500

    Создал функцию-заглушку для генерации адреса изображения аватара

--- js/map.js
+++ js/map.js
@@ -1,2 +1,30 @@
 'use strict';
 
+var NUMBER_OF_OBJECTS = 8;
+
+function generateAvatarSrc () {
+  var avatarSrc = 'img/avatars/user.png';
+  return avatarSrc;
+}
+
+function generateObject() {
+  var object = {
+    'author': {
+      'avatar': generateAvatarSrc()
+    }
+  };
+  return object;
+}
+
+function generateData(amount) {
+  var array = [];
+  for (var i = 0; i < amount; i++) {
+    array[i] = generateObject();
+    alert(array[i].author.avatar);
+  }
+  return array;
+}
+
+var data = generateData(NUMBER_OF_OBJECTS);
+
+
