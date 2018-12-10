c6daf4ef402485746369698fd329cc7b457545c3
Mikhail Shardin <kriptonit1988@gmail.com>
2018-12-10 16:06:23 +0500

    Исправил функцию createRequest

--- js/backend.js
+++ js/backend.js
@@ -5,12 +5,22 @@
   var UPLOAD_URL = 'https://js.dump.academy/keksobooking';
   var LOAD_URL = 'https://js.dump.academy/keksobooking/data';
 
-  function createRequest(errorHandler) {
+  function createRequest(loadHandler, errorHandler) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        loadHandler(xhr.response);
+      } else {
+        errorHandler('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
     xhr.addEventListener('error', function () {
       errorHandler('Произошла ошибка соединения');
     });
+
     xhr.addEventListener('timeout', function () {
       errorHandler('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
     });
@@ -22,34 +32,14 @@
 
   // загрузка данных на сервер
   function upload(data, loadHandler, errorHandler) {
-    var xhr = createRequest(errorHandler);
-
-    xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        loadHandler(xhr.response);
-      } else {
-        errorHandler('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-
+    var xhr = createRequest(loadHandler, errorHandler);
     xhr.open('POST', UPLOAD_URL);
     xhr.send(data);
   }
 
   // получение данных с сервера
   function load(loadHandler, errorHandler) {
-    var xhr = createRequest(errorHandler);
-
-    xhr.addEventListener('load', function () {
-      var result = null;
-      if (xhr.status === 200) {
-        result = loadHandler(xhr.response);
-      } else {
-        errorHandler('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-      return result;
-    });
-
+    var xhr = createRequest(loadHandler, errorHandler);
     xhr.open('GET', LOAD_URL);
     xhr.send();
   }
